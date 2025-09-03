3f4c72f0942f9bc6fa42d4ac441959bcaf3bcd99
362575441 <362575441@2260I-L29-019>
2025-09-03 09:35:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _4UTopic_One___The_Basics
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
